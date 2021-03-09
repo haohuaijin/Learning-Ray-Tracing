853bc70a98174dbf4633f98a7adf87ea1784b56d
hhj <1291536750@qq.com>
2021-03-09 08:01:15 +0800

    finish part 11 positionable camera

--- rtweekend.h
+++ rtweekend.h
@@ -16,7 +16,7 @@ const double infinity = std::numeric_limits<double>::infinity();
 const double pi = 3.1415926535897932385;
 
 // Utility Functions
-inline double degress_to_radians(double degrees){
+inline double degrees_to_radians(double degrees){
     return degrees * pi / 180.0;
 }
 
